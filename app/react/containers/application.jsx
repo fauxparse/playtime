840a6b139b2c48f9b4409284976706b9ea28b081
Matt Powell <fauxparse@gmail.com>
2016-10-30 14:21:06 +1300

    animated menu button

--- app/react/containers/application.jsx
+++ app/react/containers/application.jsx
@@ -4,6 +4,16 @@ import { Router, Route, Redirect, Link, browserHistory } from 'react-router';
 import { VelocityComponent } from 'velocity-react';
 import Sidebar from './sidebar';
 
+const MenuButton = ({ open, onClick }) => (
+  <button rel="menu" data-open={open} onClick={onClick}>
+    <svg width={24} height={24} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <rect x={3} y={6} width={18} height={2}/>
+      <rect x={3} y={11} width={18} height={2}/>
+      <rect x={3} y={16} width={18} height={2}/>
+    </svg>
+  </button>
+)
+
 class Application extends Component {
   constructor(props) {
     super(props);
@@ -15,9 +25,9 @@ class Application extends Component {
       <div id="application">
         {this.props.sidebar || <Sidebar/>}
         <VelocityComponent {...this.sidebarAnimation()}>
-          <main>
+          <main data-sidebar-open={this.state.sidebar}>
             <header>
-              <button onClick={(e) => this.toggleSidebar(e)}>Menu</button>
+              <MenuButton open={this.state.sidebar} onClick={(e) => this.toggleSidebar(e)} />
             </header>
             {this.props.content}
             {this.shim()}
