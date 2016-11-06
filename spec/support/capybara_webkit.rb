ee6d5f92aefbe1a6acaa5ef1963d615fe7a63216
Matt Powell <matt.powell@powershop.co.nz>
2016-11-07 12:13:51 +1300

    investigate poltergeist problems: try increasing timeout

--- spec/support/capybara_webkit.rb
+++ spec/support/capybara_webkit.rb
@@ -6,4 +6,9 @@
 # end
 
 require 'capybara/poltergeist'
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, timeout: 1.minute)
+end
+
 Capybara.javascript_driver = :poltergeist
