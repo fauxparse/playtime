0308caec3f402d9333af91f2849e41a2ecabc441
Matt Powell <matt.powell@powershop.co.nz>
2016-11-21 16:17:21 +1300

    fix formatting in smtp.rb

--- config/smtp.rb
+++ config/smtp.rb
@@ -4,27 +4,32 @@ if ENV.key?('MAILTRAP_API_TOKEN')
   require 'rest_client'
   require 'json'
 
-  response = RestClient::Resource.new("https://mailtrap.io/api/v1/inboxes.json?api_token=#{ENV['MAILTRAP_API_TOKEN']}", ssl_version: "TLSv1").get
+  MAILTRAP_API = 'https://mailtrap.io/api/v1/inboxes.json'
+
+  response = RestClient::Resource.new(
+    "#{MAILTRAP_API}?api_token=#{ENV['MAILTRAP_API_TOKEN']}",
+    ssl_version: 'TLSv1'
+  ).get
 
   first_inbox = JSON.parse(response)[0]
 
   SMTP_SETTINGS = {
-    :user_name => first_inbox['username'],
-    :password => first_inbox['password'],
-    :address => first_inbox['domain'],
-    :domain => first_inbox['domain'],
-    :port => first_inbox['smtp_ports'][0],
-    :authentication => :plain
+    user_name:            first_inbox['username'],
+    password:             first_inbox['password'],
+    address:              first_inbox['domain'],
+    domain:               first_inbox['domain'],
+    port:                 first_inbox['smtp_ports'][0],
+    authentication:       :plain
   }.freeze
 else
   SMTP_SETTINGS = {
-    address: ENV.fetch('SMTP_ADDRESS'), # example: "smtp.sendgrid.net"
-    authentication: :plain,
-    domain: ENV.fetch('SMTP_DOMAIN'), # example: "heroku.com"
+    address:              ENV.fetch('SMTP_ADDRESS'), # e.g. "smtp.sendgrid.net"
+    authentication:       :plain,
+    domain:               ENV.fetch('SMTP_DOMAIN'),  # e.g. "heroku.com"
     enable_starttls_auto: true,
-    password: ENV.fetch('SMTP_PASSWORD'),
-    port: '587',
-    user_name: ENV.fetch('SMTP_USERNAME')
+    password:             ENV.fetch('SMTP_PASSWORD'),
+    port:                 '587',
+    user_name:            ENV.fetch('SMTP_USERNAME')
   }.freeze
 end
 
